c35207f41581c8e473ff4cdfd02f3657ec4bc31b
Ciaran Badman <cpbadman@gmail.com>
2019-07-21 19:47:39 +0100

    Basic requester

--- initialTest.py
+++ initialTest.py
@@ -1,12 +1,18 @@
 import requests
 import json
+import yaml
+
+#accessdata = None
+
+username = 'ciaran'
 
 #OAuth for this later
-accessToken = 'o.SJYfTHRtgStE2nYhmTkJET5uCrx6vm0h'
+with open('access.yaml','r') as stream:
+	accessdata = yaml.safe_load(stream)
+accessToken = accessdata[username]
 tokenHeader = {'Access-Token':accessToken}
 
-
-r = requests.get('https://api.pushbullet.com/v2/users/me',headers = None)
+r = requests.get('https://api.pushbullet.com/v2/users/me',headers = tokenHeader)
 
 print(json.dumps(json.loads(r.text), indent = 4, ensure_ascii = False))
 
